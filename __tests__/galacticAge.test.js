d7b32d6cd1b1a4c5d82417cfc8826f8aba342999
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-10 09:09:48 -0800

    Add good fail for GalacticAge.mercury

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -1,13 +1,22 @@
 import {GalacticAge} from "../src/galacticAge.js"
 
 describe("Galactic Age", () => {
+    let galacticAge;
+    beforeEach(() => {
+        galacticAge= new GalacticAge(10)
+    });
+
+    // constructor
     test("should create a GalacticAge class that takes in a # input for age", () => {
-        const galacticAge = new GalacticAge(10);
         expect(galacticAge.age).toEqual(10);
     });
 
+    // earth
     test("should create an Earth function that assigns the age to Earth", () => {
-        const galacticAge = new GalacticAge(10);
         expect(galacticAge.earthAge).toEqual(10);
+    });
+    // mercury
+    test("should create a Mercury function that changes age compared to Earth by 0.24 yrs / 1 year", () => {
+        expect(galacticAge.mercuryAge).toEqual(41.66);
     });
 });
--- src/galacticAge.js
+++ src/galacticAge.js
@@ -2,9 +2,14 @@ export class GalacticAge {
   constructor(age) {
     this.age = age;
     this.earthAge = this.earth();
+    this.mercuryAge = this.mercury();
   }
 
   earth() {
     return this.age;
+  }
+
+  mercury() {
+    return 0;
   }
 }
